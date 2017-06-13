881b442ff376060a35bb768e75be93dcac5faf13
fakuit01 <8header8@googlemail.com>
2017-06-13 16:40:38 +0200

    vorbereitung für mehrere objekte
    alles ausser leitung

--- src/main/java/Starter/Bauelemente/Bauelemente.java
+++ src/main/java/Starter/Bauelemente/Bauelemente.java
@@ -1,5 +1,9 @@
 package Starter.Bauelemente;
 
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 
@@ -29,25 +33,216 @@ public abstract class Bauelemente {
     double snap2X=0,snap2Y=0;
 */
     int ID;
-    protected double posX = 0, posY = 0;
+    double posX = 0, posY = 0;
     int Orientation=0;
+    ImageView imageview = new ImageView();
+    boolean deleted=false;
+    BorderPane border=new BorderPane();
+    ImageView helpimage = new ImageView();
 
     //Icon
-    public Bauelemente(int ID, double posX, double posY, int Orientation) {
-        this.posX = posX;
-        this.posY = posY;
+    public Bauelemente(int ID, double posX1, double posY1, int Orientation) {
+        this.posX = posX1;
+        this.posY = posY1;
         this.ID=ID;
         this.Orientation = Orientation;
+        /*
+        //KeyEvent funktioniert nicht
+        imageview.setOnKeyPressed(new EventHandler<KeyEvent>()
+        {
+            public void handle(KeyEvent event) {
+                if (event.getCode()==KeyCode.CONTROL)
+                {
+                    System.out.println("Control Spannungsquelle");
+                }
+            }
+        });
+        */
+        //Wenn man über das Objekt drüber fährt
+        imageview.setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event) {
+                //färbt beim drüberfahren das objekt in farbe
+                orientationF();
+
+                //Hilfebild
+                helpimage.setY(border.getHeight() - 36);
+                helpimage.setX(5);
+                border.getChildren().add(helpimage);
+                //Hilfebild ende
+            }});
+        //Wenn man das Objekt verlässt
+        imageview.setOnMouseExited(new EventHandler<MouseEvent>(){
+
+            //Muelleimer
+            public void handle(MouseEvent event)
+            {
+                border.getChildren().remove(helpimage);
+                if(event.getSceneX()<=125&&event.getSceneY()>=450&&event.getSceneY()<=500) {
+                    deleted=true;
+                    imageview.setImage(null);
+                    imageview.removeEventHandler(MouseEvent.ANY, this);
+                }
+                //Transparent in schwarz je nach orientierung
+                else
+                {
+                    orientationS();
+                }
+            }});
+        //Ändert das Bild in das mit schwarzen Hintergrund beim Losllassen der Maustaste
+        imageview.setOnMouseReleased(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event)
+            {
+                if(event.getButton()== MouseButton.PRIMARY) {
+
+                    //Mülleimer Funktion löscht alle Händler und das Bild Klasse bleibt allerdings erhalten
+                    //wird auch nicht vom Borderpane entfernt bei border.getChildren().remove(imageview) kommen fehler
+                    if (event.getSceneX() <= 125 && event.getSceneY() >= 450 && event.getSceneY() <= 500) {
+                        deleted = true;
+                        imageview.setImage(null);
+                        imageview.removeEventHandler(MouseEvent.ANY, this);
+                    }
+                    //oben
+                    else if(event.getSceneY() < 50) {
+                        posX = round(event.getSceneX());
+                        posY = 50;
+                        imageview.setX(posX - 25);
+                        imageview.setY(posY-25);
+                        orientationS();
+                    }
+                    //rechts passt nicht immer
+                    else if (event.getSceneX() > border.getWidth()-40) {
+                        posX = round(border.getWidth()-40);
+                        posY = round(event.getSceneY());
+                        imageview.setX(posX-25);
+                        imageview.setY(posY-25);
+                        orientationS();
+                    }
+                    //links muss noch
+                    else if (event.getSceneX() <= 125&&event.getSceneY()<=450){
+                        posX = round(0+125+25);
+                        posY = round(event.getSceneY());
+                        imageview.setX(posX - 25);
+                        imageview.setY(posY- 25);
+                        orientationS();
+                    }
+                    //unten
+                    else if (event.getSceneY() >= (border.getHeight()-25 - 40))
+                    {
+                        posX = round(event.getSceneX());
+                        posY = round(border.getHeight()-25- 40);
+                        imageview.setX(posX-25);
+                        imageview.setY(posY-25);
+                        orientationS();
+                    }
+                    //normaler bereich
+                    else
+                    {
+                        posX = round(event.getSceneX());
+                        posY = round(event.getSceneY());
+                        imageview.setX(posX- 25);
+                        imageview.setY(posY- 25);
+                        orientationS();
+                    }
+                }else return;
+            }});
+        //zeichnet während des drag das Transparente Bild
+        imageview.setOnMouseDragged(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event)
+            {
+                if(event.getButton()==MouseButton.PRIMARY) {
+                    //beta anfang
+                    //Welches Bild ist aktuell? Wegen drehen des Bildes
+                    //oberhalb menüleiste
+                    if (event.getSceneY() < 25)
+                    {
+                        imageview.setX(event.getSceneX() - 25);
+                        orientationR();
+                    }
+                    //in linker vbox allerdings nicht beim Mülleimer
+                    // Todo man kann vom mülleimer hochziehen bug
+                    else if (event.getSceneX() < 125&&event.getSceneY()<450 )
+                    {
+                        imageview.setY(event.getSceneY() - 25);
+                        orientationR();
+                    }
+                    //untere Hbox
+                    else if (event.getSceneY() > (border.getHeight()-40)) {
+                        imageview.setX(event.getSceneX() - 25);
+                        orientationR();
+                    }
+                    //rechte vbox
+                    else if (event.getSceneX() > (border.getWidth()-25))
+                    {
+                        imageview.setY(event.getSceneY() - 25);
+                        orientationR();
+                    }
+                    else
+                    {
+                        //normaler Bereich
+                        orientationFT();
+                        imageview.setX(event.getSceneX() - 25);
+                        imageview.setY(event.getSceneY() - 25);
+                    }
+                }
+            }});
+        //Rechtsklick Drehung bzw ändern des Bildes
+        imageview.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    //Welches Bild ist aktuell? Wegen drehen des Bildes
+                    orientationFundOrienation();
+                    //System.out.println("Ori rechtsklick "+Orientation);
+                }
+                //funktioniert noch nicht richtig bei rechtsdrehung dann linksdrehung
+            /*else if(event.getButton()==MouseButton.PRIMARY)
+                {
+                    if(Orientation==0) {imageview.setImage(F135);Orientation=3;}
+                    else if(Orientation==1) {imageview.setImage(F90);Orientation=0;}
+                    else if(Orientation==2) {imageview.setImage(F45);Orientation=1;}
+                    else if(Orientation==3) {imageview.setImage(F00);Orientation=2;}
+                    System.out.println("Ori links "+Orientation);
+                }*/
+            }});
     }
 
     //public abstract void draw(GraphicsContext gc, double Orientation);
     //Zeichnen Methode
-    public abstract void draw(BorderPane borderPane);
     //Wird zum String xml hinzugefügt
     public abstract String toxml(String xml);
     public abstract double getOrientation();
     public abstract double getPosX();
     public abstract double getPosY();
+    public void orientationS(){}
+    public void orientationR(){}
+    public void orientationF(){}
+    public void orientationFT(){}
+    public void orientationFundOrienation(){}
+    //Snap ans Raster der Bauteile
+    public double round(double runden)
+    {
+        double a=0,b=0;
+        if (runden % 25 < 12.5) {
+            a= runden - (runden % 25);
+            return a;
+
+        } else if (runden % 25 >= 12.5) {
+            b= runden +  (25-runden % 25);
+            return b;
+        } else return 0;
+    }
+    //Zeichnen Methode
+    public void draw(BorderPane borderPane)
+    {
+        this.border=borderPane;
+        imageview.setX(posX-25);
+        imageview.setY(posY-25);
+        orientationS();
+        border.getChildren().add(imageview);
+    }
 
 }
 
--- src/main/java/Starter/Bauelemente/Kondensator.java
+++ src/main/java/Starter/Bauelemente/Kondensator.java
@@ -33,192 +33,15 @@ public class Kondensator extends Bauelemente {
     Image help=new Image("file:Images/kondensatorhilfe.png",900,30,false,false);
 
 
-    ImageView imageview = new ImageView();
-    ImageView helpimage = new ImageView();
-    boolean deleted=false;
-    BorderPane border=new BorderPane();
 
     public Kondensator(int ID, double x, double y, int Orientation1)
     {
         super(ID,x,y,Orientation1);
         imageview.setImage(S00);
         helpimage.setImage(help);
-        //Wenn man über das Objekt drüber fährt
-        imageview.setOnMouseEntered(new EventHandler<MouseEvent>(){
 
-            public void handle(MouseEvent event) {
-                //färbt beim drüberfahren das objekt in farbe
-                if(Orientation==0) {imageview.setImage(F00);}
-                else if(Orientation==1){imageview.setImage(F45);}
-                else if(Orientation==2){imageview.setImage(F90);}
-                else if(Orientation==3){imageview.setImage(F135);}
-                helpimage.setY(border.getHeight()-36);
-                helpimage.setX(5);
-                border.getChildren().add(helpimage);
-            }});
-        //Wenn man das Objekt verlässt
-        imageview.setOnMouseExited(new EventHandler<MouseEvent>(){
-            //Muelleimer
-            public void handle(MouseEvent event) {
-                border.getChildren().remove(helpimage);
-                if(event.getSceneX()<=125&&event.getSceneY()>=450&&event.getSceneY()<=500) {
-                    deleted=true;
-                    imageview.setImage(null);
-                    imageview.removeEventHandler(MouseEvent.ANY, this);
-                }
-                //Transparent in schwarz je nach orientierung
-                else {orientationS();}
-            }});
-        //Rechtsklick Drehung bzw ändern des Bildes
-        imageview.setOnMouseClicked(new EventHandler<MouseEvent>(){
-
-            public void handle(MouseEvent event) {
-                if (event.getButton() == MouseButton.SECONDARY) {
-                    //Welches Bild ist aktuell? Wegen drehen des Bildes
-                    if(Orientation==0) {imageview.setImage(F45);Orientation=1;}
-                    else if(Orientation==1) {imageview.setImage(F90);Orientation=2;}
-                    else if(Orientation==2) {imageview.setImage(F135);Orientation=3;}
-                    else if(Orientation==3) {imageview.setImage(F00);Orientation=0;}
-                    //System.out.println("Ori rechtsklick "+Orientation);
-                }
-                //Todo funktioniert noch nicht richtig bei rechtsdrehung dann linksdrehung
-            /*else if(event.getButton()==MouseButton.PRIMARY)
-                {
-                    if(Orientation==0) {imageview.setImage(F135);Orientation=3;}
-                    else if(Orientation==1) {imageview.setImage(F90);Orientation=0;}
-                    else if(Orientation==2) {imageview.setImage(F45);Orientation=1;}
-                    else if(Orientation==3) {imageview.setImage(F00);Orientation=2;}
-                    System.out.println("Ori links "+Orientation);
-                }*/
-            }});
-        //zeichnet während des drag das Transparente Bild
-        imageview.setOnMouseDragged(new EventHandler<MouseEvent>(){
-
-            public void handle(MouseEvent event)
-            {
-                if(event.getButton()==MouseButton.PRIMARY) {
-                    //beta anfang
-                    //Welches Bild ist aktuell? Wegen drehen des Bildes
-                    //oberhalb menüleiste
-                    if (event.getSceneY() < 25)
-                    {
-                        imageview.setX(event.getSceneX() - 25);
-                        orientationR();
-                    }
-                    //in linker vbox allerdings nicht beim Mülleimer
-                    // Todo man kann vom mülleimer hochziehen bug
-                    else if (event.getSceneX() < 125&&event.getSceneY()<450 )
-                    {
-                        imageview.setY(event.getSceneY() - 25);
-                        orientationR();
-                    }
-                    //untere Hbox
-                    else if (event.getSceneY() > (border.getHeight()-40)) {
-                        imageview.setX(event.getSceneX() - 25);
-                        orientationR();
-                    }
-                    //rechte vbox
-                    else if (event.getSceneX() > (border.getWidth()-25))
-                    {
-                        imageview.setY(event.getSceneY() - 25);
-                        orientationR();
-                    }
-                    else
-                    {
-                        //beta ende
-                        //normaler Bereich
-                        if (Orientation == 0) {imageview.setImage(FT00);}
-                        else if (Orientation == 1) {imageview.setImage(FT45);}
-                        else if (Orientation == 2) {imageview.setImage(FT90);}
-                        else if (Orientation == 3) {imageview.setImage(FT135);}
-                        imageview.setX(event.getSceneX() - 25);
-                        imageview.setY(event.getSceneY() - 25);
-                    }
-                }
-            }});
-        //Ändert das Bild in das mit schwarzen Hintergrund beim Losllassen der Maustaste
-        imageview.setOnMouseReleased(new EventHandler<MouseEvent>(){
-
-            public void handle(MouseEvent event)
-            {
-                if(event.getButton()==MouseButton.PRIMARY) {
-
-                    //Mülleimer Funktion löscht alle Händler und das Bild Klasse bleibt allerdings erhalten
-                    if (event.getSceneX() <= 125 && event.getSceneY() >= 450 && event.getSceneY() <= 500) {
-                        deleted = true;
-                        imageview.setImage(null);
-                        imageview.removeEventHandler(MouseEvent.ANY, this);
-                    }
-                    //oben
-                    else if(event.getSceneY() < 50) {
-                        posX = round(event.getSceneX());
-                        posY = 50;
-                        imageview.setX(posX - 25);
-                        imageview.setY(posY-25);
-                        orientationS();
-                    }
-                    //rechts passt nicht immer
-                    else if (event.getSceneX() > border.getWidth()-40) {
-                        posX = round(border.getWidth()-40);
-                        posY = round(event.getSceneY());
-                        imageview.setX(posX-25);
-                        imageview.setY(posY-25);
-                        orientationS();
-                    }
-                    //links muss noch
-                    else if (event.getSceneX() <= 125&&event.getSceneY()<=450){
-                        posX = round(0+125+25);
-                        posY = round(event.getSceneY());
-                        imageview.setX(posX - 25);
-                        imageview.setY(posY- 25);
-                        orientationS();
-                    }
-                    //unten
-                    else if (event.getSceneY() >= (border.getHeight()-25 - 40))
-                    {
-                        posX = round(event.getSceneX());
-                        posY = round(border.getHeight()-25- 40);
-                        imageview.setX(posX-25);
-                        imageview.setY(posY-25);
-                        orientationS();
-                    }
-                    //normaler bereich
-                    else
-                    {
-                        posX = round(event.getSceneX());
-                        posY = round(event.getSceneY());
-                        imageview.setX(posX- 25);
-                        imageview.setY(posY- 25);
-                        orientationS();
-                    }
-                }else return;
-            }});
-        /*
-        //Key drücken um Aktion durchzuführen geht nicht
-        imageviewSpannungsquelle1.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-               // if(event.getCode()==KeyCode.E) {
-                    //if(event.getCode()== KeyCode.DELETE)
-                    System.out.println("Test"+event.getText());
-                }
-            //}
-        });
-        */
     }
-    //Zeichnen Methode
-    public void draw(BorderPane borderPane)
-    {
-        this.border=borderPane;
-        imageview.setX(posX-25);
-        imageview.setY(posY-25);
-        if(Orientation==0) {imageview.setImage(S00);}
-        else if(Orientation==1){imageview.setImage(S45);}
-        else if(Orientation==2){imageview.setImage(S90);}
-        else if(Orientation==3){imageview.setImage(S135);}
-        else imageview.setImage(S00);
-        border.getChildren().add(imageview);
-    }
+
     //Wird zum String xml hinzugefügt
     public String toxml(String xml){
         if(deleted==false) {
@@ -232,19 +55,6 @@ public class Kondensator extends Bauelemente {
             return xml;
         }
         else return xml;
-    }
-    //Snap ans Raster der Bauteile
-    public double round(double runden)
-    {
-        double a=0,b=0;
-        if (runden % 25 < 12.5) {
-            a= runden - (runden % 25);
-            return a;
-
-        } else if (runden % 25 >= 12.5) {
-            b= runden +  (25-runden % 25);
-            return b;
-        } else return 0;
     }
     public  double getOrientation() {return Orientation;}
     public  double getPosX() {return posX;}
@@ -262,6 +72,32 @@ public class Kondensator extends Bauelemente {
         else if (Orientation == 1) {imageview.setImage(R45);}
         else if (Orientation == 2) {imageview.setImage(R90);}
         else if (Orientation == 3) {imageview.setImage(R135);}
+    }
+    public void orientationF()
+    {
+        if (Orientation == 0) {
+            imageview.setImage(F00);
+        } else if (Orientation == 1) {
+            imageview.setImage(F45);
+        } else if (Orientation == 2) {
+            imageview.setImage(F90);
+        } else if (Orientation == 3) {
+            imageview.setImage(F135);
+        }
+    }
+    public void orientationFT()
+    {
+        if (Orientation == 0) {imageview.setImage(FT00);}
+        else if (Orientation == 1) {imageview.setImage(FT45);}
+        else if (Orientation == 2) {imageview.setImage(FT90);}
+        else if (Orientation == 3) {imageview.setImage(FT135);}
+    }
+    public void orientationFundOrienation()
+    {
+        if(Orientation==0) {imageview.setImage(F45);Orientation=1;}
+        else if(Orientation==1) {imageview.setImage(F90);Orientation=2;}
+        else if(Orientation==2) {imageview.setImage(F135);Orientation=3;}
+        else if(Orientation==3) {imageview.setImage(F00);Orientation=0;}
     }
 }
 
