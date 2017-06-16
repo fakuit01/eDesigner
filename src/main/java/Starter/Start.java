fcbb1e7d5ede27867aed88f299385d5c49c53ecd
fakuit01 <8header8@googlemail.com>
2017-06-16 12:29:12 +0200

    splashscreen und startscreen gelöscht

--- src/main/java/Starter/Start.java
+++ src/main/java/Starter/Start.java
@@ -2,13 +2,7 @@ package Starter;
 
 public class Start {
     public static void main(String[] args) {
-        //Startscreen.execute(args);
-        /*
-        Startscreen startscreen=new Startscreen();
-        startscreen.showscreen();
-        startscreen.closescreen();
-*/
-        //new Splashscreen();
+        
         App.execute(args);
 
     }
